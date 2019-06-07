dc2ccd161214300c8f115e62d75860a0af86795b
ferrison <ne4ehbka98@gmail.com>
2019-06-07 13:54:57 +0300

    v0.01b

---
+++ Assets/Shake.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Shake : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float accelerometerUpdateInterval = 1.0f / 60.0f;
+    // The greater the value of LowPassKernelWidthInSeconds, the slower the
+    // filtered value will converge towards current input sample (and vice versa).
+    float lowPassKernelWidthInSeconds = 1.0f;
+    // This next parameter is initialized to 2.0 per Apple's recommendation,
+    // or at least according to Brady! ;)
+    float shakeDetectionThreshold = 2.0f;
+
+    float lowPassFilterFactor;
+    Vector3 lowPassValue;
+
+    Text myText;
+
+    float dTime;
+
+    void Start()
+    {
+        lowPassFilterFactor = accelerometerUpdateInterval / lowPassKernelWidthInSeconds;
+        shakeDetectionThreshold *= shakeDetectionThreshold;
+        lowPassValue = Input.acceleration;
+
+        myText = GameObject.Find("Shake").GetComponent<Text>();
+        dTime = 0;
+    }
+
+    void Update()
+    {
+        dTime += Time.deltaTime;
+        Vector3 acceleration = Input.acceleration;
+        lowPassValue = Vector3.Lerp(lowPassValue, acceleration, lowPassFilterFactor);
+        Vector3 deltaAcceleration = acceleration - lowPassValue;
+
+        if(myText.enabled && dTime > 2)
+        {
+            myText.enabled = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        if (deltaAcceleration.sqrMagnitude >= shakeDetectionThreshold)
+        {
+            dTime = 0;
+            myText.enabled = true;
+        }
+    }
+}
